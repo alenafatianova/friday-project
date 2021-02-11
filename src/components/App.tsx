0de73d4e46bf48ce2a861449a8d55cef26891cf8
Alena Fatianova <ravemybody@gmail.com>
2021-02-11 19:51:26 +0100

    add token for reset password

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/App.module.css';
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Redirect, Route } from 'react-router-dom'
 import { Login } from './Login';
 import { SignUp } from './SignUp';
 import { Profile } from './Profile';
@@ -11,6 +11,7 @@ import { Header } from './Header';
 import React from 'react';
 import { Navlink } from './Navlink';
 
+
 export const App = () => {
   return (
     <HashRouter>
@@ -18,12 +19,13 @@ export const App = () => {
             <div><Header/></div>
             <Navlink/>
             <div className={styles.navlinkStyle}> 
+           
             <Route exact path={'/login'} component={Login} />
             <Route exact path={'/signup'} component={SignUp} />
             <Route exact path={'/profile'} component={Profile}/>
             <Route exact path={'/404'} component={ Page404 }/>
             <Route exact path={'/forgotPassword'} component={ ForgotPassword }/>
-            <Route exact path={'/resetPassword*'} component={ ResetPassword }/>
+            <Route exact path={'/resetPassword/:resetPasswordToken'} component={ ResetPassword }/>
             <Route exact path={'/test'} component={ TestPage }/>
             </div>
       </div>
--- src/redux/reducers/forgot-password-reducer.ts
+++ src/redux/reducers/forgot-password-reducer.ts
@@ -1,32 +1,24 @@
 import { forgotPasswordAPI } from '../../api/forgot-password-api';
 import { Dispatch } from "redux";
-import { useHistory } from 'react-router-dom';
 
-
-export const initialForgotPasswordState = {
+export const initialForgotPasswordState: initialForgotPasswordStateType = {
     email: '',
-    newPassword: ''
+    newPassword: '',
+    isRequestLoading: ''
+}
+
+export type initialForgotPasswordStateType = {
+    email: string
+    newPassword: string
+    isRequestLoading: RequestLoadingType
 }
 
 const SEND_EMAIL = 'friday-project/forgot-password-reducer/SEND_EMAIL';
 const RESET_PASSWORD = 'friday-project/forgot-password-reducer/RESET_PASSWORD' 
+const IS_REQUEST_LOADING = 'friday-project/forgot-password-reducer/IS_REQUEST_LOADING'
 
 
-export type sendEmailPayloadType = {
-    email: string 
-}
-
-export type sendEmailType = {
-    type: typeof SEND_EMAIL
-    payload: sendEmailPayloadType
-}
-
-export type resetPasswordType = {
-    type: typeof RESET_PASSWORD
-    payload: string
-}
-
-export const forgotPasswordReducer = (state = initialForgotPasswordState, action: actionsType) => {
+export const forgotPasswordReducer = (state: initialForgotPasswordStateType = initialForgotPasswordState, action: actionsType) => {
     switch(action.type) {
         case SEND_EMAIL: {
             return {
@@ -40,32 +32,46 @@ export const forgotPasswordReducer = (state = initialForgotPasswordState, action
                 newPassword: action.newPassword
             }
         }
+        case IS_REQUEST_LOADING: {
+            return {
+                ...state,
+                isRequestLoading: action.isRequestLoading
+            }
+        }
         default: 
             return state;
     }
 }
 
 
+export type RequestLoadingType = 'Loading' | 'Succeeded' | ''
+
+
 //--- Action Creators ------
 export const sendEmail = (email: string) => ({type: SEND_EMAIL, email} as const)
 export const resetPassword = (newPassword: string) => ({type: RESET_PASSWORD, newPassword} as const)
-
-type actionsType =  | ReturnType<typeof sendEmail>
-                    | ReturnType<typeof resetPassword>
+export const isRequestLoading = (isRequestLoading: RequestLoadingType) => ({type: IS_REQUEST_LOADING, isRequestLoading} as const)
 
 
 
 // ------ thunk for dispatching action creator ---------
 export const sendEmailThunk = (email: string) => (dispatch: Dispatch) => {
+    dispatch(isRequestLoading('Loading'))
     const from = "test-front-admin <alyena.fatianova@gmail.com>"
     const message = `<div style="background-color: lime; padding: 15px">
     password recovery link: 
     <a href='http://localhost:3000/#/resetPassword/$token$'>link</a> </div>`
     forgotPasswordAPI.forgotPassword(email, from, message)
-    dispatch(sendEmail(email)) 
+    dispatch(sendEmail(email))  
 }
 
-export const setNewPasswordThunk = (newPassword: string, resetPasswordToken: string) => (dispatch: Dispatch) => {
+export const setNewPasswordThunk = (newPassword: string, resetPasswordToken: string) => (dispatch: Dispatch) => { 
     forgotPasswordAPI.setNewPassword(newPassword, resetPasswordToken) 
     dispatch(resetPassword(newPassword))
 }
+
+
+type actionsType =  | ReturnType<typeof sendEmail>
+                    | ReturnType<typeof resetPassword>
+                    | ReturnType<typeof isRequestLoading>
+
